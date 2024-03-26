19a45a8e5f318acce9f0ee3b2df54a68ad0e2dae
Usman Ahsan <usman.ahsan@vidizmo.com>
2024-03-26 16:42:56 +0500

    update gitignore

--- SenderHost.cs
+++ SenderHost.cs
@@ -31,13 +31,13 @@ public class SenderHost:BackgroundService
             var i = 0;
             _logger.Verbose("Sender started");
             
-            Console.WriteLine("Define number of hops/minute :");
+            Console.WriteLine("Define number of Events/minute :");
             var hops=Convert.ToInt32(Console.ReadLine());
         
             while (true)
             {
-                Console.WriteLine("Sending Hop");
-                await broker.Publish("Hop : "+ i++);
+                Console.WriteLine("Sending Event");
+                await broker.Publish(i++ + "{\r\n    \"id\": 1171979,\r\n    \"addedBy\": {\r\n        \"id\": 655837\r\n    },\r\n    \"author\": {\r\n        \"firstName\": \"Huda\",\r\n        \"lastName\": \"Rauf\",\r\n        \"id\": 0\r\n    },\r\n    \"tenant\": {\r\n        \"id\": 52700,\r\n        \"tenantName\": \"multi-language\",\r\n        \"subDomainUrl\": \"multi-language.beta.vidizmo.com\",\r\n        \"isSSLEnable\": true,\r\n        \"weight\": 0,\r\n        \"uuid\": \"6ddb148b-8e63-4678-8c74-9952ddd19e51\"\r\n    },\r\n    \"estimatedDuration\": 1079908,\r\n    \"title\": \"Language-Check\",\r\n    \"version\": 13,\r\n    \"addedDate\": \"2024-03-25T08:52:56.88Z\",\r\n    \"updatedDate\": \"2024-03-25T11:12:24.507Z\",\r\n    \"updatedBy\": {\r\n        \"id\": 0\r\n    },\r\n    \"size\": 24722945,\r\n    \"publishedOn\": \"2024-03-25T08:52:56.88Z\",\r\n    \"defaultViewingAccess\": \"None\",\r\n    \"status\": \"Published\",\r\n    \"weight\": 0,\r\n    \"format\": \"Folder\",\r\n    \"player\": {\r\n        \"id\": 0,\r\n        \"displayTimedData\": false,\r\n        \"isVideo360\": false\r\n    },\r\n    \"order\": 0,\r\n    \"inheritParentAccessRights\": true,\r\n    \"storageAccessTier\": \"None\",\r\n    \"lastAccessTime\": \"2024-03-25T11:12:21.08Z\",\r\n    \"lastTierUpdateTime\": \"2024-03-25T08:52:56.88Z\",\r\n    \"uuid\": \"db3feae3-9794-4ae4-ad87-9cda5fb791a3\"\r\n}");
                 await Task.Delay((60*1000)/hops,stoppingToken);   
             }
             return ;
